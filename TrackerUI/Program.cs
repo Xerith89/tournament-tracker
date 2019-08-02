77d2443a0e7f5e73a917bf304817bbe57b7fe57e
Alex Coupe <acoupe@gmail.com>
2019-08-02 22:23:56 +0100

    Final form added

--- TrackerUI/Program.cs
+++ TrackerUI/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +16,7 @@ namespace TrackerUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TournamentViewerForm());
+            Application.Run(new TournamentDashboardForm());
         }
     }
 }
